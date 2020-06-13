5fe3e7db2b9a8fbac3ca9e412063713ca399a8c6
Paulo Melo <peimelo@gmail.com>
2020-06-13 17:35:07 -0300

    Pagina de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
