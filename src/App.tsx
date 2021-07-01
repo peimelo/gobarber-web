5e0dea1c80c43a80698f5b7e466aa49d1d21f4f6
Paulo Melo <peimelo@gmail.com>
2021-07-01 19:35:41 -0300

    Adds validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import SignIn from './pages/SignIn';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <AuthContext.Provider value={{ name: 'Melo' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,40 +1,73 @@
-import React from 'react';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import React, { useCallback, useContext, useRef } from 'react';
 import { FiLock, FiLogIn, FiMail } from 'react-icons/fi';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
+import getValidationErrors from '../../utils/getValidationErrors';
 import { Background, Container, Content } from './styles';
+import AuthContext from '../../context/AuthContext';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-      <form>
-        <h1>Faça seu logon</h1>
+  const auth = useContext(AuthContext);
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      formRef.current?.setErrors({});
 
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
 
-        <Button type="submit">Entrar</Button>
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+      const errors = getValidationErrors(err);
 
-      <a href="signup">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
 
-    <Background />
-  </Container>
-);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="signup">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default SignIn;
